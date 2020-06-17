55f6da2cf0332c8957daa1964bd5345c6b08ac6b
Michael Nava <M1cha3l.nava@gmail.com>
2020-06-17 16:50:21 -0400

    got search button working

--- assets/logic.js
+++ assets/logic.js
@@ -1,2 +1,21 @@
-console.log("Hello World");
-var previousSearches = "";
+/* console.log("Hello World"); */
+var pastSearchesLi = "";
+
+$(document).ready(function () {
+  /* console.log("Hello"); */
+  /* creating the api call url */
+  $(".btn").on("click", function (event) {
+    event.preventDefault();
+    console.log("search button click");
+  });
+});
+
+/* Storage
+    var cityName = $("#searchField").text();
+    var queryUrl =
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&units=imperialAPPID=46fd2cf224ef39969b7d55ab7f27f1e6";
+
+    console.log(cityName);
+  */
